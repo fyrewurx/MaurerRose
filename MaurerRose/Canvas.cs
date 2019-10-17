0b3594a1968ef570e736ae093ee54bd46bca70f7
Alex Me <fyrewurx666@gmail.com>
2019-10-17 19:31:10 +0300

    Add project files.

---
+++ MaurerRose/Canvas.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace MaurerRose
+{
+    public partial class Canvas : UserControl
+    {
+        /// <summary>
+        /// Points to hold data for draw lines
+        /// </summary>
+        List<Point> _points = new List<Point>();
+
+        /// <summary>
+        /// Pen used for the Monroe
+        /// </summary>
+        private Pen _penMonroe = new Pen(Color.MediumVioletRed, 1);
+        
+        /// <summary>
+        /// Pen used for the sin
+        /// </summary>
+        private Pen _penSin = new Pen(Color.Orange, 3);
+
+        /// <summary>
+        /// Brush used for canvas
+        /// </summary>
+        private SolidBrush _drawBrush = new SolidBrush(Color.Orange);
+
+        /// <summary>
+        /// Timer for animation
+        /// </summary>
+        private Timer _animationTimer = new Timer();
+
+        /// <summary>
+        /// Random for animation
+        /// </summary>
+        private Random _rnd = new Random();
+
+        private double n = 3;
+        private double d = 47;
+
+        public Canvas()
+        {
+            InitializeComponent();
+
+            DoubleBuffered = true;
+
+            _animationTimer.Interval = 1000;
+            _animationTimer.Tick += _animationTimer_Tick;
+            //_animationTimer.Start();
+        }
+
+        /// <summary>
+        /// Factor to fill canvas
+        /// </summary>
+        private int _sizeFactor = 1;
+
+        private void _animationTimer_Tick(object sender, EventArgs e)
+        {
+            n += _rnd.NextDouble();
+            d += _rnd.NextDouble() ;
+            this.Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            // Fancy graphics
+            e.Graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            e.Graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+
+            // Fresh start
+            _points.Clear();
+
+            // Calculation for translation
+            int middleX = this.Width / 2;
+            int middleY = this.Height / 2;
+            
+            // Calculate for Maurer
+            for (float i = 0; i <= 361; i += 1f)
+            {
+                double k = i * d;
+                double r = Math.Sin(DegreeToRadian(n * k)) * _sizeFactor;
+
+                int x = (int)(r * Math.Sin(DegreeToRadian(k))) + middleX;
+                int y = (int)(r * Math.Cos(DegreeToRadian(k))) + middleY;
+
+                _points.Add(new Point(x, y));
+            }
+
+            // Draw the Maurer rose
+            e.Graphics.DrawLines(_penMonroe, _points.ToArray());
+
+            // Fresh start
+            _points.Clear();
+
+            for (float i = 0; i <= 361; i += 1f)
+            {
+                float k = i;
+                double r = Math.Sin(DegreeToRadian(n * k)) * _sizeFactor;
+
+                int x = (int)(r * Math.Sin(DegreeToRadian(k))) + middleX;
+                int y = (int)(r * Math.Cos(DegreeToRadian(k))) + middleY;
+
+                _points.Add(new Point(x, y));
+            }
+
+            // Draw the main path
+            e.Graphics.DrawLines(_penSin, _points.ToArray());
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            _sizeFactor = (int)((this.Height + this.Width) / 2) / 3;
+            this.Invalidate();
+        }
+
+        /// <summary>
+        /// Get the radian angle from degrees
+        /// </summary>
+        /// <param name="angle">Angle in degrees</param>
+        /// <returns>Angle in radian</returns>
+        public double DegreeToRadian(float angle)
+        {
+            return Math.PI * angle / 180f;
+        }
+
+        /// <summary>
+        /// Get the radian angle from degrees
+        /// </summary>
+        /// <param name="angle">Angle in degrees</param>
+        /// <returns>Angle in radian</returns>
+        public double DegreeToRadian(double angle)
+        {
+            return Math.PI * angle / 180d;
+        }
+    }
+}
---
+++ MaurerRose/MainForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace MaurerRose
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.MainCanvas = new MaurerRose.Canvas();
+            this.MainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MainPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MainPanel.Controls.Add(this.MainCanvas);
+            this.MainPanel.Location = new System.Drawing.Point(12, 12);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(426, 426);
+            this.MainPanel.TabIndex = 0;
+            // 
+            // MainCanvas
+            // 
+            this.MainCanvas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MainCanvas.BackColor = System.Drawing.Color.White;
+            this.MainCanvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MainCanvas.Location = new System.Drawing.Point(3, 3);
+            this.MainCanvas.Name = "MainCanvas";
+            this.MainCanvas.Size = new System.Drawing.Size(418, 418);
+            this.MainCanvas.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 450);
+            this.Controls.Add(this.MainPanel);
+            this.Name = "MainForm";
+            this.Text = "MaurerRose";
+            this.MainPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MainPanel;
+        private Canvas MainCanvas;
+    }
+}
+
